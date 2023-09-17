0b07fde85207b89cb596599c7057d5154c97823b
Arthur Mitsuo <77021980+ArthurMitsuo@users.noreply.github.com>
2023-09-17 13:10:19 -0300

    segundo commit, inicio realizado na aplicação. Problemas para resolver a variável

--- app/src/main/java/com/example/cronometro/MainActivity.kt
+++ app/src/main/java/com/example/cronometro/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.cronometro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+    var chronometer : Chronometer;
+    var running : Boolean = true;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        chronometer = findViewById(R.id.chronometer);
+    }
+
+    fun startChronometer( ){
+        if(!running){
+            chronometer.start();
+            running = true;
+        }
+    }
+    fun pauseChronometer( ){
+        if(running){
+            chronometer.stop();
+            running = false;
+        }
+    }
+    fun resetChronometer( ){
+
     }
 }
